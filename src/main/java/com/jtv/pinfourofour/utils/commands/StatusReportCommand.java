917b77cbdf5016ac355c88d1a94c76af100abd2b
Max Wright <max.wright@jtv.com>
2019-06-25 09:25:07 -0400

    Add a required fileName parameter to status command.

--- src/main/java/com/jtv/pinfourofour/utils/commands/StatusReportCommand.java
+++ src/main/java/com/jtv/pinfourofour/utils/commands/StatusReportCommand.java
@@ -5,12 +5,20 @@ import com.beust.jcommander.Parameters;
 
 @Parameters(
         commandNames = "status",
-        commandDescription = "Check status reports of all links."
+        commandDescription = "Check status reports of all links.",
+        separators = "="
 )
 public class StatusReportCommand {
     @Parameter(
-            names = {"-f", "--filter"},
+            names = {"-e", "--external"},
             description = "Filter external links."
     )
     public boolean filterExternal = false;
+
+    @Parameter(
+            names = {"-f", "--filename"},
+            description = "File that contains the pin data to check.",
+            required = true
+    )
+    public String fileName;
 }
